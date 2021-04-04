f4ba735bbdebf176aaf473f3d6e1164a908f195d
hara-yuto <hara_yyy0421@outlook.jp>
2021-04-04 13:54:44 +0900

    【導入】シンプルカレンダー

---
+++ db/migrate/20210404044046_create_events.rb
@@ -0,0 +1,9 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.text :content,null: false
+      t.datetime :start_time
+      t.timestamps
+    end
+  end
+end
